6807e9bb0d97af564d76a5da747393211726a25c
jacobkmlalu <144668246+jacobkmlalu@users.noreply.github.com>
2023-09-11 14:30:35 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float r,area,perimeter,pi;
+printf("enter the radius of circle");
+scanf(" %f ", &r);
+perimeter= 2*3.14* r;
+area = 3.14* r* r;
+printf(" area=%f\n perimeter=%f",area,perimeter);
+return 0;
+}
